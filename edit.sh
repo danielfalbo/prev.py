b8bf369f835686bdedadd5c2e61cb75af9798dba
Daniel <39460524+danielfalbo@users.noreply.github.com>
2026-01-18 20:24:47 +0000

    fix: escape quotes in default weblog HTML

--- edit.sh
+++ edit.sh
@@ -101,7 +101,7 @@ if [ "$EXISTS" -eq "0" ]; then
     fi
 
     # Insert the new row with default HTML.
-    DEFAULT_HTML="<span style='opacity: 0.8; font-size: 0.8rem;' id='dateage'></span>"
+    DEFAULT_HTML='<span style="opacity: 0.8; font-size: 0.8rem;" id="dateage"></span>'
     if [ -n "$CREATED_TIME" ]; then
       sqlite3 $DB "INSERT INTO weblog (slug, title, html, created_time, listed) VALUES ('$SLUG', '$DISPLAY_VAL', '$DEFAULT_HTML', '$CREATED_TIME', $LISTED);"
     else
